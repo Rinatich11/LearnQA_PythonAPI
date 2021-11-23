fbfa29189240161602debf643a3817757c10b0ea
Rinatich11 <rinatinho93@gmail.com>
2021-11-23 23:01:59 +0300

    Ex13

--- Ex13.py
+++ Ex13.py
@@ -5,7 +5,8 @@ import requests
 user_agents = [
         (
             {
-                'user_agent': 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+                'user_agent': 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) '
+                              'AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
                 'platform': 'Mobile',
                 'browser': 'No',
                 'device': 'Android'
@@ -13,7 +14,8 @@ user_agents = [
         ),
         (
             {
-                'user_agent': 'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+                'user_agent': 'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                              'like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
                 'platform': 'Mobile',
                 'browser': 'Chrome',
                 'device': 'iOS'
@@ -30,7 +32,8 @@ user_agents = [
         ),
         (
             {
-                'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
+                'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
                 'platform': 'Web',
                 'browser': 'Chrome',
                 'device': 'No'
@@ -38,7 +41,8 @@ user_agents = [
         ),
         (
             {
-                'user_agent': 'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+                'user_agent': 'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                              'like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
                 'platform': 'Mobile',
                 'browser': 'No',
                 'device': 'iPhone'
